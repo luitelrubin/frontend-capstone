cbfafa957b53124fa0ca957a9ab450a5bdcf56cc
Rubin <rubinprasadluitel@gmail.com>
2025-09-16 12:07:40 +0545

    import header, main and footer components and incorporate them in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <p>Homepage</p>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
